a41a1401d1fa9446431e46554144177c28abd92f
geraldo <contato.gneto@gmail.com>
2022-05-29 04:32:59 -0300

    mobile version ok

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function onScroll() {
-    if (scrollY > 40) navigation.classList.add('scroll')
-    else navigation.classList.remove('scroll')
+    if (scrollY > 0) nav.classList.add('scroll')
+    else nav.classList.remove('scroll')
 }
 
 /* criado uma função chamada onScroll, onde pega a posição do scroll no eixo Y e compara na condição do if. Essa condição serve para dizer à aplicação quando adicionar a class scroll ao html e quando remover, fazendo com que o navigation da página mude suas propriedades de acordo com os eventos
